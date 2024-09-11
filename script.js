0747f245c08b099d373a24a48b9c0e4fbaefc06e
erwinapz <151448101+erwinapz@users.noreply.github.com>
2024-09-11 00:06:10 -0400

    Primer commit
    Adicion de archivos

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('¡Mensaje enviado con éxito!');
+    // Aquí agregar la lógica para enviar el formulario usando fetch o Ajax
+});
+
